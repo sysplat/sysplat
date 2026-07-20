a082369a2c67add797431e87c0f7626ace5b4b1e
Parsa Samandi <parsasamandizadeh@gmail.com>
2026-07-20 14:44:42 -0700

    chore: ignore and remove .wrangler tmp files

--- .wrangler/tmp/dev-YT5aUe/__next-on-pages-dist__/manifest/__RSC_SERVER_MANIFEST.js
+++
@@ -1,5 +0,0 @@
-var n={},t=(_,e,o)=>(n.__RSC_SERVER_MANIFEST=_.__RSC_SERVER_MANIFEST=`{
-  "node": {},
-  "edge": {},
-  "encryptionKey": "UG6pBJOA6/QWlR1/f0YYQ1nnzTw+lXDGholYRp2hp8o="
-}`,n);export{t as __getNamedExports};
